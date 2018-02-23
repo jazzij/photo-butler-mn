4627b13851391753b3b2d2fc7e95d71fb2be922e
Prateek Vachher <vachh007@umn.edu>
2018-02-23 17:04:09 -0600

    Changed to Just find faces on multi

--- image_node.py
+++ image_node.py
@@ -4,19 +4,3 @@ from AWSS3 import *
 dataset = []
 for x in list_directory_s3('pictures'):
     save_find_faces.delay(x)
-    break
-
-while True:
-    print ('PROCESSING')
-    flag = 0
-    for x in dataset:
-        if x.ready():
-            flag = 1
-        else:
-            flag = 0
-        break
-    break
-    if flag == 1:
-        print 'Images Classified'
-        break
-
