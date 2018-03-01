b4a41dc2111ff836c757a1bbcb9a401be646d8b9
Prateek Vachher <vachh007@umn.edu>
2018-03-01 00:56:20 -0600

    Working Public Release

---
+++ PhotoProject.py
@@ -0,0 +1,57 @@
+import face_recognition
+import os
+import sys
+import csv
+from PIL import Image
+
+
+class PhotoProject:
+    
+    def __init__(self):
+        try:
+            print ("Modules Successfully Imported")
+        except:
+            print ("Import Error")
+    
+    def save_find_faces(self,filename):
+        image = face_recognition.load_image_file(filename)
+        face_locations = face_recognition.face_locations(image)
+        img = Image.open(filename)
+        counter = 0
+        filename = filename.split('/')[-1]
+
+        for x in face_locations:
+            img2 = img.crop((x[3],x[0],x[1],x[2]))
+            img2.save('faces/'+str(counter)+filename)
+            counter += 1
+            
+    def compare_faces(self,face1, face2):
+        try:
+            picture_of_me = face_recognition.load_image_file('./faces/'+face1)
+        except:
+            print ("Error Loading Image 1")
+            sys.exit()
+        
+
+        try:        
+            unknown_picture = face_recognition.load_image_file('./faces/'+face2)
+        except:
+            print ("Error Loading Image 2")
+            sys.exit()
+
+
+        try:
+            my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+        except:
+            print ("Error Encoding Image 1")
+            sys.exit()
+
+
+        try:
+            unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+        except:
+            print ("Error Encoding Image 2")
+            sys.exit()
+        
+        results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
+        return results[0]
