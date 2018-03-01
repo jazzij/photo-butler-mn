90bd9498512a4f704cd28557622446f829145b31
jazzij <jazzij@umich.edu>
2018-03-01 10:54:25 -0600

    Adding comments

--- PhotoProject.py
+++ PhotoProject.py
@@ -12,7 +12,8 @@ class PhotoProject:
             print ("Modules Successfully Imported")
         except:
             print ("Import Error")
-    
+    """ Automatically saves any found face into a folder. Naming convention is [x][photoID].jpg (ie 01138, 11138, 21138)
+    """
     def save_find_faces(self,filename):
         image = face_recognition.load_image_file(filename)
         face_locations = face_recognition.face_locations(image)
@@ -24,7 +25,10 @@ class PhotoProject:
             img2 = img.crop((x[3],x[0],x[1],x[2]))
             img2.save('faces/'+str(counter)+filename)
             counter += 1
-            
+    
+    """ Compare distance between two faces to see if its the same person. .500 is optimal threshold (per face rec library, referencing MIT algorithm)
+        Below .5 means its the same person, and above means its not. 
+    """
     def compare_faces(self,face1, face2):
         try:
             picture_of_me = face_recognition.load_image_file('./faces/'+face1)
