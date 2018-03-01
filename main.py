d4167399c7ecf957a233e5f18ddda58f26872a65
Prateek Vachher <vachh007@umn.edu>
2018-03-01 09:57:31 -0600

    Random new commit

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from PhotoProject import PhotoProject
 
 classobject = PhotoProject()
 
-classobject.save_find_faces("./pictures/"+"189.jpg")
+classobject.save_find_faces("./pictures/"+"stat.jpg")
 
 distance = classobject.compare_faces("0189.jpg","1189.jpg")
-
+distance2 = classobject.compare_faces("0189.jpg","0hqdefault.jpg")
+print distance2
 print ("Distance between the two Images: "+ str(distance))
