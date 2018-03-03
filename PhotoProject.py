00e59a09bc7634eef9a4b4f524b54d10ae4cf5cc
Prateek Vachher <vachh007@umn.edu>
2018-03-03 16:48:10 -0600

    New Find Faces

--- PhotoProject.py
+++ PhotoProject.py
@@ -5,19 +5,22 @@ import csv
 from tqdm import tqdm
 from PIL import Image
 from shutil import copyfile
-
+from multiprocessing import Process
+from multiprocessing import Pool
 class PhotoProject:
     
     def __init__(self):
         try:
+            print ("")
             print ("Modules Successfully Imported")
         except:
+            print ("")
             print ("Import Error")
     
-    def save_find_faces(self,filename):
-        image = face_recognition.load_image_file(filename)
-        face_locations = face_recognition.face_locations(image)
-        img = Image.open(filename)
+    def save_find_faces(self,filename, accessor=""):
+        image = face_recognition.load_image_file(accessor+filename)
+        face_locations = face_recognition.face_locations(image, model="hog")
+        img = Image.open(accessor+filename)
         counter = 0
         filename = filename.split('/')[-1]
 
@@ -30,47 +33,30 @@ class PhotoProject:
         print ("")
         print ("== Detecting Faces in Images ==")
         print ("")
+        pool = []
+        print ("Distributing Processes")
         for x in tqdm(os.listdir("./pictures")):
-            image = face_recognition.load_image_file("./pictures/"+x)
-            face_locations = face_recognition.face_locations(image)
-            img = Image.open("./pictures/"+x)
-            counter = 0
-            filename = x.split('/')[-1]
-
-            for x in face_locations:
-                img2 = img.crop((x[3],x[0],x[1],x[2]))
-                img2.save('faces/nonclustered/'+str(counter)+filename)
-                counter += 1
-            #print ("Successfully Found Faces in "+x)
+            p = Process(target=self.save_find_faces, args=(x,"./pictures/",))
+            p.start()
+            pool.append(p)
+        print ("Processing Images")
+        for x in tqdm(pool):
+            x.join()
 
     def compare_faces(self,face1, face2):
         try:
             picture_of_me = face_recognition.load_image_file('./faces/nonclustered/'+face1)
+            my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
         except:
             print ("Error Loading Image 1")
             sys.exit()
-        
-
-        try:        
+        try:         
             unknown_picture = face_recognition.load_image_file('./faces/nonclustered/'+face2)
+            unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
         except:
             print ("Error Loading Image 2")
             sys.exit()
 
-
-        try:
-            my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
-        except:
-            print ("Error Encoding Image 1")
-            sys.exit()
-
-
-        try:
-            unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-        except:
-            print ("Error Encoding Image 2")
-            sys.exit()
-        
         results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
         return results[0]
     
@@ -84,50 +70,30 @@ class PhotoProject:
         ifile  = open('facedata.csv', "wb")
         writer = csv.writer(ifile)
         writer.writerow(['Picture1','Picture2','Distance'])
-
-        good = 0
-        bad = 0
-        badfiles = []
+        data = []
+        names = []
+        bad = []
         a = os.listdir('./faces/nonclustered/')
         print ("")
         print ("== Comparing Faces ==")
         print ("")
+        print ("Encoding Faces")
         for x in tqdm(range(len(a))):
-            for y in range(x,len(a)):
-                if a[x] == a[y] or a[x] in badfiles or a[y] in badfiles or a[x] == "clustered" or a[y] == "clustered":
-                    pass
-                else:
-                    try:
-                        picture_of_me = face_recognition.load_image_file('./faces/nonclustered/'+a[x])
-                    except:
-                        badfiles.append(a[x])
-
-                    try:  
-                        unknown_picture = face_recognition.load_image_file('./faces/nonclustered/'+a[y])
-                    except:
-                        badfiles.append(a[y])
-                        break
-                    
-                    try:
-                        my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
-                    except:
-                        badfiles.append(a[x])
-
-                    try:
-                        unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-                    except:
-                        badfiles.append(a[y])
-                        break
-                    try:
-                        results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
-                        writer.writerow([a[x],a[y],str(results[0])])
-                        good += 1            
-                    except:
-                        bad += 1
-            #print ("Successfully Processed Image #"+str(x))
-
-        print (badfiles)
-        for x in badfiles:
+            try:
+                picture_of_me =  face_recognition.load_image_file('./faces/nonclustered/'+a[x])
+                data.append(face_recognition.face_encodings(picture_of_me)[0])
+                names.append(a[x])
+            except:
+                bad.append(a[x])
+        print ("Comparing Faces")
+        for y in tqdm(range(len(data))):
+            for z in range(y+1, len(data)):    
+                try:
+                    results = face_recognition.face_distance([data[y]], data[z]) 
+                    writer.writerow([names[y],names[z],str(results[0])])
+                except:
+                    continue
+        for x in bad:
             os.remove("./faces/nonclustered/"+x)
 
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PhotoProject import PhotoProject
 
 classobject = PhotoProject()
-#classobject.save_find_faces("./pictures/"+"stat.jpg")
+#classobject.save_find_faces("./pictures/"+"q2f1.png")
+#classobject.save_find_faces("./pictures/"+"q2f2.png")
 
 #distance = classobject.compare_faces("0189.jpg","1189.jpg")
-#distance2 = classobject.compare_faces("0189.jpg","0hqdefault.jpg")
+#distance2 = classobject.compare_faces("0q2f1.png","0q2f2.png")
 #print distance2
 #print ("Distance between the two Images: "+ str(distance))
 classobject.save_find_faces_all()
 classobject.compare_all_faces()
-classobject.cluster_faces()
+#classobject.cluster_faces()
