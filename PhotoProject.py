8b537ec4c36685aa90c3bb8ee25c40574423195a
Prateek Vachher <vachh007@umn.edu>
2018-03-08 12:36:11 -0600

    Seems to be stable + COMMENTED CODE :D

--- PhotoProject.py
+++ PhotoProject.py
@@ -4,7 +4,11 @@ from PIL import Image
 from shutil import copyfile
 from multiprocessing import Process, Pool, cpu_count
 
+# ---------------------------------------------------#
+
 class PhotoProject:
+    
+# ---------------------------------------------------#
     
     def __init__(self):
         try:
@@ -14,72 +18,83 @@ class PhotoProject:
             print ("")
             print ("Import Error")
     
-
+# ---------------------------------------------------#
 
     def save_find_faces(self,filename, accessor=""):
-        if type(filename)=="str":
-            filename = [filename]
-        for z in (filename):
-            image = face_recognition.load_image_file(accessor+z)
-            face_locations = face_recognition.face_locations(image, model="hog")
-            img = Image.open(accessor+z)
-            counter = 0
-            z = z.split('/')[-1]
+        try:
+            if type(filename)=="str":
+                filename = [filename]
+            for z in (filename):
+                image = face_recognition.load_image_file(accessor+z)
+                face_locations = face_recognition.face_locations(image)
+                img = Image.open(accessor+z)
+                counter = 0
+                z = z.split('/')[-1]
 
-            for x in face_locations:
-                img2 = img.crop((x[3],x[0],x[1],x[2]))
-                img2.save('faces/nonclustered/'+str(counter)+z)
-                counter += 1
-    
+                for x in face_locations:
+                    img2 = img.crop((x[3],x[0],x[1],x[2]))
+                    img2.save('faces/nonclustered/'+str(counter)+z)
+                    counter += 1
+            return True
+
+        except:
+            print ("Found Error, Crash Error Code 100")
+            return False
+            
+# ---------------------------------------------------#
 
     def line_split(self, N, K=1):
         length = len(N)
         return [N[i*length/K:(i+1)*length/K] for i in range(K)]
 
-
+# ---------------------------------------------------#
 
     def save_find_faces_all(self):
-        start_time = time.time()
+        try:
+            print ("")
+            print ("== Detecting Faces in Images ==")
+            print ("")
+            pool = []
+            dat = (os.listdir("./pictures"))
+            chunked = self.line_split(dat,cpu_count()-1)
+            for y in chunked:         
+                p1 = Process(target=self.save_find_faces, args=(y,"./pictures/",))
+                pool.append(p1)
+            for y in pool:
+                y.start()
+            print ("Processing Started")
+            for y in pool:
+                y.join()
+            print ("Successfully Identified Faces")
+            print ("")
+            return True
+        except:
+            print ("Found Error, Crash Error Code 101")
+            return False
 
-        print ("")
-        print ("== Detecting Faces in Images ==")
-        print ("")
-        pool = []
-        dat = (os.listdir("./pictures"))
-        chunked = self.line_split(dat,cpu_count()-1)
-        for y in chunked:         
-            p1 = Process(target=self.save_find_faces, args=(y,"./pictures/",))
-            pool.append(p1)
-        for y in pool:
-            y.start()
-        print ("Processing Started")
-
-        for y in pool:
-            y.join()
-        print ("Successfully Identified Faces")
-        print("--- %s seconds ---" % (time.time() - start_time))
-        print ("")
-
-
+# ---------------------------------------------------#
 
     def compare_faces(self,face1,face2):
         try:
-            picture_of_me = face_recognition.load_image_file('./faces/nonclustered/'+face1)
-            my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+            try:
+                picture_of_me = face_recognition.load_image_file('./faces/nonclustered/'+face1)
+                my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+            except:
+                print ("Error Loading Image 1")
+                return -1
+            try:         
+                unknown_picture = face_recognition.load_image_file('./faces/nonclustered/'+face2)
+                unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+            except:
+                print ("Error Loading Image 2")
+                return -1
+            results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
+            return results[0]
         except:
-            print ("Error Loading Image 1")
-            sys.exit()
-        try:         
-            unknown_picture = face_recognition.load_image_file('./faces/nonclustered/'+face2)
-            unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-        except:
-            print ("Error Loading Image 2")
-            sys.exit()
+            return -1
+            print ("Found Error, Crash Error Code 102")
 
-        results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
-        return results[0]
-
-
+# ---------------------------------------------------#
 
     def check_existance(self,branch,to_search1,to_search2):
         for x in range(len(branch)):
@@ -87,43 +102,54 @@ class PhotoProject:
                 return (x)
         return (-1)
 
-
+# ---------------------------------------------------#
 
     def compare_all_faces(self):
-        start_time = time.time()
-        ifile  = open('facedata.csv', "wb")
-        writer = csv.writer(ifile)
-        writer.writerow(['Picture1','Picture2','Distance'])
-        data = []
-        names = []
-        bad = []
-        a = os.listdir('./faces/nonclustered/')
-        print ("")
-        print ("== Comparing Faces ==")
-        print ("")
-        print ("Encoding Faces")
-        for x in tqdm(range(len(a))):
-            try:
-                picture_of_me =  face_recognition.load_image_file('./faces/nonclustered/'+a[x])
-                data.append(face_recognition.face_encodings(picture_of_me)[0])
-                names.append(a[x])
-            except:
-                bad.append(a[x])
-        print ("Comparing Faces")
-        for y in tqdm(range(len(data))):
-            for z in range(y+1, len(data)):    
+        try:
+            # Initializing Variables and Data Types
+            ifile  = open('facedata.csv', "wb")
+            writer = csv.writer(ifile)
+            data,name,badfiles = [],[],[]
+
+            # Writing Initial Row to CSV
+            writer.writerow(['Picture1','Picture2','Distance'])
+
+            # Process all files in nonclustered
+            available_files = os.listdir('./faces/nonclustered/')
+
+            print ("")
+            print ("== Comparing Faces ==")
+            print ("")
+            print ("Encoding Faces")
+
+            # Encoding all files to memory
+            for x in tqdm(range(len(available_files))):
                 try:
-                    results = face_recognition.face_distance([data[y]], data[z]) 
-                    writer.writerow([names[y],names[z],str(results[0])])
+                    image_file =  face_recognition.load_image_file('./faces/nonclustered/'+available_files[x])
+                    data.append(face_recognition.face_encodings(image_file)[0])
+                    names.append(available_files[x])
                 except:
-                    continue
+                    badfiles.append(available_files[x])
+            
+            # Comparing the encoded files
+            print ("Comparing Faces")
+            for y in tqdm(range(len(data))):
+                for z in range(y+1, len(data)):    
+                    try:
+                        results = face_recognition.face_distance([data[y]], data[z]) 
+                        writer.writerow([names[y],names[z],str(results[0])])
+                    except:
+                        continue
 
-        for x in bad:
-            os.remove("./faces/nonclustered/"+x)
-        print("--- %s seconds ---" % (time.time() - start_time))
-        print ("")
+            # Removing Bad Face Encoded Files
+            for x in badfiles:
+                os.remove("./faces/nonclustered/"+x)
 
+        except:
+            return -1
+            print ("Found Error, Crash Error Code 103")
 
+# ---------------------------------------------------#
 
     def check_exist(self,branch,file1):
         for x in branch:
@@ -131,44 +157,53 @@ class PhotoProject:
                 return True
         return False
 
-
+# ---------------------------------------------------#
 
     def cluster_faces(self,threshold=0.5):
-        ifile  = open('facedata.csv', "rb")
-        writer = csv.reader(ifile)
-        branch = []
+        try:
+            # Initializing Variables and Data Types
+            ifile  = open('facedata.csv', "rb")
+            writer = csv.reader(ifile)
+            branch = []
+            folder_id = 0
 
-        for x in writer:
-            try:
-                if float(x[-1]) <= threshold:
-                    tag = self.check_existance(branch,x[0],x[1])
+            # Iterating through CSV File
+            for x in writer:
+                try:
+                    if float(x[-1]) <= threshold:       # Evaluating Face Distance with Threshold
+                        tag = self.check_existance(branch,x[0],x[1])   # Checking Existance in any pre-existing branch
+                        if tag >= 0:
+                            if x[0] in branch[tag]:
+                                branch[tag].append(x[1])
+                            elif x[1] in branch[tag]:
+                                branch[tag].append(x[0])
+                        else:
+                            branch.append([x[0],x[1]])
+                except:
+                    pass
 
-                    if tag >= 0:
-                        if x[0] in branch[tag]:
-                            branch[tag].append(x[1])
-                        elif x[1] in branch[tag]:
-                            branch[tag].append(x[0])
-                    else:
-                        branch.append([x[0],x[1]])
-            except:
-                pass
-        counter = 0
-        for x in branch:
-            if str(counter) in os.listdir("./faces/clustered/"):
-                pass
-            else:
-                os.mkdir("./faces/clustered/"+str(counter))
-            for y in x:
-                copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(counter)+"/"+y)
-            counter += 1
 
-        for y in os.listdir('./faces/nonclustered/'):
-            if self.check_exist(branch,y):
-                pass
-            else:
-                if str(counter) in os.listdir("./faces/clustered/"):
+            # Sorting Facial Images with Existing Branch Classifications
+            for x in branch:
+                if str(folder_id) in os.listdir("./faces/clustered/"):
                     pass
                 else:
-                    os.mkdir("./faces/clustered/"+str(counter))
-                copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(counter)+"/"+y)
-                counter += 1
+                    os.mkdir("./faces/clustered/"+str(folder_id))
+                for y in x:
+                    copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(folder_id)+"/"+y)
+                folder_id += 1
+
+
+            # Generating Branches for Independent Facial Images
+            for y in os.listdir('./faces/nonclustered/'):
+                if self.check_exist(branch,y)==False:
+                    if str(folder_id) not in os.listdir("./faces/clustered/"):
+                        os.mkdir("./faces/clustered/"+str(folder_id))
+                    copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(folder_id)+"/"+y)
+                    folder_id += 1
+                
+        except:
+            return -1
+            print ("Found Error, Crash Error Code 104")
+        
+# ---------------------------------------------------#
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from PhotoProject import PhotoProject
 
 classobject = PhotoProject()
-#classobject.save_find_faces("./pictures/"+"q2f1.png")
-#classobject.save_find_faces("./pictures/"+"q2f2.png")
 
-#distance = classobject.compare_faces("0189.jpg","1189.jpg")
-#distance2 = classobject.compare_faces("0q2f1.png","0q2f2.png")
-#print distance2
-#print ("Distance between the two Images: "+ str(distance))
+# Available Modules
+classobject.save_find_faces(filename,'./pictures/')
 classobject.save_find_faces_all()
+classobject.compare_faces(face1,face2)
 classobject.compare_all_faces()
-#classobject.cluster_faces()
+classobject.cluster_faces()
