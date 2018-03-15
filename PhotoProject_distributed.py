a9144a25f2baa99451707768e2d09cddb94456cc
Prateek Vachher <vachh007@umn.edu>
2018-03-15 15:17:19 -0500

    Progress

---
+++ PhotoProject_distributed.py
@@ -0,0 +1,206 @@
+import face_recognition, os, sys, time, csv
+from tqdm import tqdm
+from PIL import Image
+from shutil import copyfile
+from multiprocessing import Process, Pool, cpu_count
+
+# ---------------------------------------------------#
+ ''' 
+    Automatically saves any found face into a folder. 
+    Naming convention is [x][photoID].jpg (ie 01138, 11138, 21138)
+ '''
+
+def save_find_faces(filename, accessor=""):
+    try:
+        if type(filename)=="str":
+            filename = [filename]
+        for z in (filename):
+            image = face_recognition.load_image_file(accessor+z)
+            face_locations = face_recognition.face_locations(image)
+            img = Image.open(accessor+z)
+            counter = 0
+            z = z.split('/')[-1]
+
+            for x in face_locations:
+                img2 = img.crop((x[3],x[0],x[1],x[2]))
+                img2.save('faces/nonclustered/'+str(counter)+z)
+                counter += 1
+        return True
+
+    except:
+        print ("Found Error, Crash Error Code 100")
+        return False
+            
+# ---------------------------------------------------#
+
+def line_split(N, K=1):
+    length = len(N)
+    return [N[i*length/K:(i+1)*length/K] for i in range(K)]
+
+# ---------------------------------------------------#
+
+def save_find_faces_all():
+    try:
+        print ("")
+        print ("== Detecting Faces in Images ==")
+        print ("")
+        pool = []
+        dat = (os.listdir("./pictures"))
+        chunked = line_split(dat,cpu_count()-1)
+        for y in chunked:         
+            p1 = Process(target=save_find_faces, args=(y,"./pictures/",))
+            pool.append(p1)
+        for y in pool:
+            y.start()
+        print ("Processing Started")
+        for y in pool:
+            y.join()
+        print ("Successfully Identified Faces")
+        print ("")
+        return True
+    except:
+        print ("Found Error, Crash Error Code 101")
+        return False
+
+# ---------------------------------------------------#
+
+ ''' 
+    Compare distance between two faces to see if its the same person. 
+    .45 is optimal threshold (per face rec library, referencing CMU OpenFace algorithm)
+    Below .45 means its the same person, and above means its not. 
+'''
+def compare_faces(face1,face2):
+    try:
+        try:
+            picture_of_me = face_recognition.load_image_file('./faces/nonclustered/'+face1)
+            my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+        except:
+            print ("Error Loading Image 1")
+            return -1
+        try:         
+            unknown_picture = face_recognition.load_image_file('./faces/nonclustered/'+face2)
+            unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+        except:
+            print ("Error Loading Image 2")
+            return -1
+        results = face_recognition.face_distance([my_face_encoding], unknown_face_encoding) 
+        return results[0]
+    except:
+        print ("Found Error, Crash Error Code 102")
+        return -1
+
+# ---------------------------------------------------#
+
+def compare_all_faces():
+    try:
+        # Initializing Variables and Data Types
+        ifile  = open('facedata.csv', "wb")
+        writer = csv.writer(ifile)
+        data,names,badfiles = [],[],[]
+
+        # Writing Initial Row to CSV
+        writer.writerow(['Picture1','Picture2','Distance'])
+
+        # Process all files in nonclustered
+        available_files = os.listdir('./faces/nonclustered/')
+
+        print ("")
+        print ("== Comparing Faces ==")
+        print ("")
+        print ("Encoding Faces")
+
+        # Encoding all files to memory
+        for x in tqdm(range(len(available_files))):
+            try:
+                image_file =  face_recognition.load_image_file('./faces/nonclustered/'+available_files[x])
+                data.append(face_recognition.face_encodings(image_file)[0])
+                names.append(available_files[x])
+            except:
+                badfiles.append(available_files[x])
+        
+        # Comparing the encoded files
+        print ("Comparing Faces")
+        for y in tqdm(range(len(data))):
+            for z in range(y+1, len(data)):    
+                try:
+                    results = face_recognition.face_distance([data[y]], data[z]) 
+                    writer.writerow([names[y],names[z],str(results[0])])
+                except:
+                    continue
+        print ("Number of Bad Faces ", len(badfiles))
+        # Removing Bad Face Encoded Files
+        for x in badfiles:
+            os.remove("./faces/nonclustered/"+x)
+
+    except:
+        print ("Found Error, Crash Error Code 103")
+        return -1
+
+# ---------------------------------------------------#
+
+def check_exist(branch,file1):
+    for x in branch:
+        if file1 in x:
+            return True
+    return False
+
+# ---------------------------------------------------#
+
+def check_existance(branch,to_search1,to_search2):
+    for x in range(len(branch)):
+        if to_search1 in branch[x] or to_search2 in branch[x]:
+            return (x)
+    return (-1)
+
+# ---------------------------------------------------#
+
+def cluster_faces(threshold=0.45):
+    try:
+        # Initializing Variables and Data Types
+        ifile  = open('facedata.csv', "rb")
+        writer = csv.reader(ifile)
+        branch = []
+        folder_id = 0
+
+        # Iterating through CSV File
+        for x in writer:
+            try:
+                if float(x[-1]) <= threshold:       # Evaluating Face Distance with Threshold
+                    tag = check_existance(branch,x[0],x[1])   # Checking Existance in any pre-existing branch
+                    if tag >= 0:
+                        if x[0] in branch[tag]:
+                            if check_exist(branch,x[1])==False:
+                                branch[tag].append(x[1])
+                        elif x[1] in branch[tag]:
+                            if check_exist(branch,x[0])==False:
+                                branch[tag].append(x[0])
+                    else:
+                        branch.append([x[0],x[1]])
+            except:
+                pass
+
+
+        # Sorting Facial Images with Existing Branch Classifications
+        for x in branch:
+            if str(folder_id) in os.listdir("./faces/clustered/"):
+                pass
+            else:
+                os.mkdir("./faces/clustered/"+str(folder_id))
+            for y in x:
+                copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(folder_id)+"/"+y)
+            folder_id += 1
+
+
+        # Generating Branches for Independent Facial Images
+        for y in os.listdir('./faces/nonclustered/'):
+            if check_exist(branch,y)==False:
+                if str(folder_id) not in os.listdir("./faces/clustered/"):
+                    os.mkdir("./faces/clustered/"+str(folder_id))
+                copyfile("./faces/nonclustered/"+y, "./faces/clustered/"+str(folder_id)+"/"+y)
+                folder_id += 1
+            
+    except:
+        print ("Found Error, Crash Error Code 104")
+        return -1
+        
+# ---------------------------------------------------#
---
+++ mongocommands.py
@@ -0,0 +1,50 @@
+from pymongo import *
+import gridfs,os
+
+def send_file_mongo(fileloc,filename,database):
+    db = MongoClient()[database]
+    fs = gridfs.GridFS(db)
+    try:
+        if filename in list_directory_mongo(database):
+            print ("File Exists")
+        else:
+            file_to_send = open(fileloc,'rb')
+            fs.put(file_to_send,filename=filename)
+            return True
+    except:
+        print ("Error 101")
+
+def list_directory_mongo(database):
+    try:
+        db = MongoClient()[database]
+        fs = gridfs.GridFS(db)
+        return (fs.list())
+    except:
+        print ("Error 102")
+
+def get_file_mongo(filename,database):
+    try:
+        db = MongoClient()[database]
+        fs = gridfs.GridFS(db)
+        for demo in fs.find({"filename": filename}):
+            a = open(filename,'wb')
+            a.write(demo.read())
+            a.close()
+        return True
+    except:
+        print ("Error 103")
+
+def send_all_images_mongo(foldername, database):
+    try:
+        for x in os.listdir('./'+foldername+'/'):
+            if x != '.DS_Store':
+                send_file_mongo('./'+foldername+'/'+x,x,database)
+        return True
+    except:
+        print ("Error 104")
+
+
+#send_file_mongo('IMG_1222.JPG','IMG_1222.JPG','photoproject')
+#print (list_directory_mongo('photoproject'))
+#get_file_mongo('IMG_1222.JPG','photoproject')
+#send_all_images_mongo('pictures','photoproject')
