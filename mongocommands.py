e0201817a19d4d0008d27585ccdaebeba6c358f1
Prateek Vachher <vachh007@umn.edu>
2018-03-21 14:22:57 -0500

    New Mongo IP

--- mongocommands.py
+++ mongocommands.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 # ---------------------------------------------------#
 
-mongo = "10.128.198.120"            # IP Address of MongoDB Host
+mongo = "10.147.117.77"            # IP Address of MongoDB Host
 
 # ---------------------------------------------------#
 
