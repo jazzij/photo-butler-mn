9bfa90e27c9a3fbc997d70eb4a0adaee7e589a94
jazzij <jazzij@umich.edu>
2018-06-27 13:59:17 -0500

    Created facefinder.py

---
+++ facefinder.py
@@ -0,0 +1,6 @@
+"""
+Summer 2018 
+1) Add working functions here
+2) Put your name in comments of functions that you contribute
+3) Maintain similarity in variable naming with prior posted code, unless you see some egregious errors.
+"""
