459fa90d554567e6f0b076fdbf7fd8741b5b549b
Lily <lily130472@gmail.com>
2018-06-28 17:47:45 -0500

    add find_and()

--- facefinder.py
+++ facefinder.py
@@ -90,3 +90,46 @@ def make_clean(dirPath='./pictures/'):
         os.rmdir('./sorted_out/')
     if os.path.isdir('./found_person/'):
         rmtree('./found_person/')
+    if os.path.isdir('./find_and/'):
+        rmtree('./find_and/')
+
+# Find set AND (Lily)
+# Given a photo with a set of people, finds all photos that contain every person
+# in the set and copies them into a separate directory.
+# I haven't been able to get 100% accuracy even with a higher threshold; it
+# might be that the pictures I'm using to test are too different?
+# Parameters: path to the photo containing the subjects; path to the directory
+# containing a set of photos
+def find_and(setPath, dirPath='./pictures/'):
+    if dirPath[-1] != '/':
+        dirPath += '/'
+    if not os.path.isdir('./find_and/'):
+        os.mkdir('./find_and/')
+    setImg = face_recognition.load_image_file(setPath)
+    setEncodings = face_recognition.face_encodings(setImg)
+    if len(setEncodings) == 0:
+        print("No faces were found in the set.")
+        return
+    print("{} faces were found in the set.".format(len(setEncodings)))
+    for picture in tqdm(os.listdir(dirPath)):
+        # Creates a list of boolean values for each test photo. Each boolean
+        # represents one of the faces we're looking for. Once it's found, it
+        # flips to True. Once every value in the switchboard is True, we know
+        # the photo matches the set we're looking for.
+        switchboard = [False] * len(setEncodings)
+        testPath = dirPath + picture
+        testImg = face_recognition.load_image_file(testPath)
+        testEncodings = face_recognition.face_encodings(testImg)
+        if len(testEncodings) == 0:
+            continue
+        # For each person in the subject photo, check if they are in the test
+        # photo
+        for i in range(len(setEncodings)):
+            distances = face_recognition.face_distance(testEncodings, \
+            setEncodings[i])
+            for d in distances:
+                if d <= 0.51:
+                    switchboard[i] = True
+                    break
+        if False not in switchboard:
+            copy(testPath, './find_and/')
