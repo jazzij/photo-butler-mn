72259322550f38d744b7ddd43f09263355858384
Lily <lily130472@gmail.com>
2018-06-27 22:33:15 -0500

    add highlight_faces()

--- facefinder.py
+++ facefinder.py
@@ -1,6 +1,29 @@
 """
-Summer 2018 
+Summer 2018
 1) Add working functions here
 2) Put your name in comments of functions that you contribute
 3) Maintain similarity in variable naming with prior posted code, unless you see some egregious errors.
 """
+
+import face_recognition, cv2
+
+# Highlight faces in a photo (Lily)
+# Draws red rectangles around each identified face
+# Parameters: path to photo (e.g. './pictures/my_photo.jpg')
+def highlight_faces(imgPath):
+    # Loads the image first into face_recognition to find the faces, then to cv2
+    # to draw rectangles
+    img = face_recognition.load_image_file(imgPath)
+    cvimg = cv2.imread(imgPath, 1)
+
+    face_locations = face_recognition.face_locations(img)
+    # Each item in face_locations is the dimensions of a box around a face
+    # We can pass these dimensions into the cv2 rectangle function to highlight
+    for face in face_locations:
+        top, right, bottom, left = face
+        cv2.rectangle(cvimg, (left, top), (right, bottom), (0,0,255), 5)
+
+    # Display image
+    cv2.imshow("highlighted faces", cvimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
