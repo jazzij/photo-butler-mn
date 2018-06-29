d2262daba43eb61bb83f2fd8382b288dc2eb8511
HeavenDog <buixx212@umn.edu>
2018-06-29 10:54:11 -0500

    second script

---
+++ binh/project2.py
@@ -0,0 +1,34 @@
+import os
+import face_recognition
+def or_operator():
+    picture = input("Enter the name of the file: ")
+    img = face_recognition.load_image_file(picture)
+    img_encoding = face_recognition.face_encodings(img)
+    res = []
+    for i in img_encoding:
+        for j in os.listdir():
+            if j != "project2.py":
+                img = face_recognition.load_image_file(j)
+                img_encoding = face_recognition.face_encodings(img)
+                result = face_recognition.compare_faces(img_encoding,i)
+                if True in result:
+                    if not j in res:
+                        res.append(j)
+    return res
+def not_operator():
+    picture = input("Enter the name of the file: ")
+    img = face_recognition.load_image_file(picture)
+    img_encoding = face_recognition.face_encodings(img)
+    res = []
+    for i in img_encoding:
+        for j in os.listdir():
+            if j != "project2.py":
+                img = face_recognition.load_image_file(j)
+                img_encoding = face_recognition.face_encodings(img)
+                result1 = face_recognition.compare_faces(img_encoding,original_img[0])
+                result2 = face_recognition.compare_faces(img_encoding,i)
+                if True in result1:
+                    if not True in result2:
+                        if not j in res:
+                            res.append(j)
+    return res
