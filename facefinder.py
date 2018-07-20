6de2b4fed250995e5a9b7de18d623290a38addd9
binhbui0805 <thaibinh4k3@gmail.com>
2018-07-19 19:45:27 -0500

    Update facefinder.py

--- facefinder.py
+++ facefinder.py
@@ -6,6 +6,7 @@ Summer 2018
 """
 
 import face_recognition, cv2, os, pickle
+import time
 from tqdm import tqdm
 from shutil import move, copy, rmtree
 from colorsys import hsv_to_rgb
@@ -305,3 +306,113 @@ def scrub(scrubPath, dirPath='./pictures/', encoded=True):
         # unchanged
         if not found:
             copy(testPath, './no_scrubs/')
+#Applying "or" operation to all the faces in a given picture (Binh)
+#Return a list of all the images that contain any person in the given picture
+#Create a folder that contains of those image files.
+def or_operator(path,picture):
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
+    result_list = []
+    for i in os.listdir(path):
+        if i != "project2.py":
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
+            boolean_list = []
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
+                if True in result:
+                    boolean_list.append(True)
+                else:
+                    boolean_list.append(False)
+            if True in boolean_list:
+                result_list.append(i)
+                if os.path.exists(path+"or_operation"):
+                    copy(path+i,path+"or_operation\\"+i)
+                else:
+                    os.mkdir(path+"or_operation")
+                    copy(path+i,path+"or_operation\\"+i)
+    return result_list
+#Applying "not" operation to all the faces in a given picture(Binh)
+#Return a list of images containing  the first face of the given picture but not containing any other faces in the given picture.
+#Create a folder containing all those images
+def not_operator(path,picture):
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
+    result_list = []
+    for i in os.listdir(path):
+        if i != "project2.py":
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
+            boolean_list = []
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
+                if True in result:
+                    boolean_list.append(True)
+                else:
+                    boolean_list.append(False)
+            if True == boolean_list[0]:
+                if not True in boolean_list:
+                    result_list.append(i)
+                    if os.path.exists(path+"not_operation"):
+                        copy(path+i,path+"not_operation\\"+i)
+                    else:
+                        os.mkdir(path+"not_operation")
+                        copy(path+i,path+"not_operation\\"+i)
+    return result_list
+#An integrated version of all the four operations that have been written so far(Binh)
+def nested_operator(path,picture,operations):
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
+    result_list = []
+    for i in os.listdir(path):
+        if i != "project2.py":
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
+            boolean_list = []
+            final_boolean = True
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
+                if True in result:
+                    boolean_list.append(True)
+                else:
+                    boolean_list.append(False)
+            for k in range(len(boolean_list)-1):
+                final_boolean = boolean_list[0]
+                if k >= len(operations):
+                    operator = operations[-1]
+                else:
+                    operator = operations[k]
+                if operator.lower() == "and":
+                    final_boolean = final_boolean and boolean_list[k+1]
+                elif operator.lower() == "or":
+                    final_boolean = final_boolean or boolean_list[k+1]
+                elif operator.lower() == "not":
+                    if boolean_list[k+1] == True:
+                        final_boolean = False
+                    else:
+                        final_boolean = True
+                elif operator.lower() == "xor":
+                    if final_boolean == boolean_list[k+1]:
+                        final_boolean = False
+                    else:
+                        final_boolean = True
+            if final_boolean == True:
+                result_list.append(i)
+                if os.path.exists(path+"nested_operations"):
+                    copy(path+i,path+"nested_operations\\"+i)
+                else:
+                    os.mkdir(path+"nested_operations")
+                    copy(path+i,path+"nested_operations\\"+i)
+    return result_list
+#Print out the list of the files in a certain directory in the order of the times they were created(Binh) 
+def timeline(path):
+    result = []
+    for i in os.listdir(path):
+        file_info = os.stat(i)
+        timeData = time.localtime(file_info.st_ctime)
+        readableTime = time.ctime(file_info.st_ctime)
+        tupleData = (timeData,i,readableTime)
+        result.append(tupleData)
+    ordered_result = sorted(result)
+    for j in fin_res:
+        print(str(j[-2])+" "+str(j[-1]))
