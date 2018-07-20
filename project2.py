7a436ac529a7892029b555a22fd9cf7cb7c4cb02
binhbui0805 <thaibinh4k3@gmail.com>
2018-07-19 19:09:35 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -2,52 +2,67 @@ import os
 import face_recognition
 from shutil import copy
 def or_operator(path,picture):
-    img = face_recognition.load_image_file(path+picture)
-    img_encoding = face_recognition.face_encodings(img)
-    result_list = []
-    for i in img_encoding:
-        for j in os.listdir(path):
-            if j != "project2.py":
-                image = face_recognition.load_image_file(j)
-                image_encoding = face_recognition.face_encodings(image)
-                result = face_recognition.compare_faces(image_encoding,i)
-                if True in result:
-                    if not j in result_list:
-                        result_list.append(j)
-                        copy(path+j,path+"/or_operator/"+j)
-    return result_list
-def not_operator(path,picture):
-    img = face_recognition.load_image_file(path+picture)
-    img_encoding = face_recognition.face_encodings(img)
-    result_list = []
-    for i in img_encoding:
-        for j in os.listdir(path):
-            if j != "project2.py":
-                image = face_recognition.load_image_file(j)
-                image_encoding = face_recognition.face_encodings(image)
-                result1 = face_recognition.compare_faces(image_encoding,original_img[0])
-                result2 = face_recognition.compare_faces(image_encoding,i)
-                if True in result1:
-                    if not True in result2:
-                        if not j in result_list:
-                            result_list.append(j)
-                            copy(path+j,path+"/not_operator/"+j)
-    return result_list
-def nested_operator(path,picture,operations):
-    img = face_recognition.load_image_file(path+picture)
-    img_encoding = face_recognition.face_encodings(img)
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
     result_list = []
     for i in os.listdir(path):
         if i != "project2.py":
-            image = face_recognition.load_image_file(path+i)
-            image_encoding = face_recognition.face_encodings(image)
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
             boolean_list = []
-            final_boolean = True
-            for j in img_encoding:
-                result = face_recognition.compare_faces(image_encoding,j)
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
                 if True in result:
                     boolean_list.append(True)
-                if not True in result:
+                else:
+                    boolean_list.append(False)
+            if True in boolean_list:
+                result_list.append(i)
+                if os.path.exists(path+"or_operation"):
+                    copy(path+i,path+"or_operation\\"+i)
+                else:
+                    os.mkdir(path+"or_operation")
+                    copy(path+i,path+"or_operation\\"+i)
+    return result_list
+def not_operator(path,picture):
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
+    result_list = []
+    for i in os.listdir(path):
+        if i != "project2.py":
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
+            boolean_list = []
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
+                if True in result:
+                    boolean_list.append(True)
+                else:
+                    boolean_list.append(False)
+            if True == boolean_list[0]:
+                if not True in boolean_list:
+                    result_list.append(i)
+                    if os.path.exists(path+"not_operation"):
+                        copy(path+i,path+"not_operation\\"+i)
+                    else:
+                        os.mkdir(path+"not_operation")
+                        copy(path+i,path+"not_operation\\"+i)
+    return result_list
+def nested_operator(path,picture,operations):
+    original_img = face_recognition.load_image_file(path+picture)
+    original_img_encoding = face_recognition.face_encodings(original_img)
+    result_list = []
+    for i in os.listdir(path):
+        if i != "project2.py":
+            img = face_recognition.load_image_file(path+i)
+            img_encoding = face_recognition.face_encodings(img)
+            boolean_list = []
+            final_boolean = True
+            for j in original_img_encoding:
+                result = face_recognition.compare_faces(img_encoding,j)
+                if True in result:
+                    boolean_list.append(True)
+                else:
                     boolean_list.append(False)
             for k in range(len(boolean_list)-1):
                 final_boolean = boolean_list[0]
@@ -77,4 +92,3 @@ def nested_operator(path,picture,operations):
                     os.mkdir(path+"nested_operations")
                     copy(path+i,path+"nested_operations\\"+i)
     return result_list
-
